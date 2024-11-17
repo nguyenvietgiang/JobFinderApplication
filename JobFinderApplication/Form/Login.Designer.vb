d267e1979815c1f353af3a6b54fcf4f8a532efe2
Giang <nguyenvietgiang1110@gmail.com>
2024-11-17 21:24:07 +0700

    Using webview 2 to show web in wiform

--- JobFinderApplication/Form/Login.Designer.vb
+++ JobFinderApplication/Form/Login.Designer.vb
@@ -24,30 +24,46 @@ Partial Class Login
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
         PictureBox1 = New PictureBox()
+        WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' PictureBox1
         ' 
         PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
-        PictureBox1.Location = New Point(269, 77)
+        PictureBox1.Location = New Point(518, 12)
         PictureBox1.Name = "PictureBox1"
         PictureBox1.Size = New Size(236, 70)
         PictureBox1.TabIndex = 0
         PictureBox1.TabStop = False
         ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.White
+        WebView21.Location = New Point(12, 88)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(1233, 479)
+        WebView21.TabIndex = 1
+        WebView21.ZoomFactor = 1R
+        ' 
         ' Login
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1257, 579)
+        Controls.Add(WebView21)
         Controls.Add(PictureBox1)
         Name = "Login"
         Text = "Form1"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
 
 End Class
--- JobFinderApplication/Form/Login.vb
+++ JobFinderApplication/Form/Login.vb
@@ -1,5 +1,14 @@
-﻿Public Class Login
-    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports Microsoft.Web.WebView2.Core
 
+Public Class Login
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebView21.Source = New Uri("http://www.fithou.edu.vn/")
     End Sub
+
+    Private Sub WebView21_NavigationStarting(sender As Object, e As CoreWebView2NavigationStartingEventArgs) Handles WebView21.NavigationStarting
+        MessageBox.Show("Đang tải trang: " & e.Uri)
+    End Sub
+
 End Class
+
+
