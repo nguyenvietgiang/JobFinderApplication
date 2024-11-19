3ac5306c84523e45ea95d54039fe03b85e6ba68e
Giang <nguyenvietgiang1110@gmail.com>
2024-11-19 21:05:55 +0700

    Lần thứ 1 Test

--- JobFinderApplication/Connection/DatabaseHelper.vb
+++ JobFinderApplication/Connection/DatabaseHelper.vb
@@ -1,4 +1,8 @@
 ﻿Public Class DatabaseHelper
+    ''' <summary>
+    ''' ConnectionString (Lần thứ 1)
+    ''' </summary>
+    ''' <returns></returns>
     Public Shared ReadOnly Property ConnectionString As String
         Get
             Return "Data Source=DESKTOP-GOAKFLS\SQLEXPRESS;Initial Catalog=GoldenJobDB;Integrated Security=True; TrustServerCertificate=True"
--- JobFinderApplication/Form/Login.vb
+++ JobFinderApplication/Form/Login.vb
@@ -5,6 +5,11 @@ Public Class Login
         WebView21.Source = New Uri("http://www.fithou.edu.vn/")
     End Sub
 
+    ''' <summary>
+    ''' Đây là sử lý để load web (Lần thứ 1)
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub WebView21_NavigationStarting(sender As Object, e As CoreWebView2NavigationStartingEventArgs) Handles WebView21.NavigationStarting
         MessageBox.Show("Đang tải trang: " & e.Uri)
     End Sub
